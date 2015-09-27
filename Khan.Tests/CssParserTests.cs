01e6c3f27574624881d1152ad68affa58804809b
Maxim Soloviev <max@max2.me>
2015-09-27 10:13:13 -0700

    Added project with unit tests.

---
+++ Khan.Tests/CssParserTests.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Khan.Tests
+{
+	[TestFixture]
+	public class CssParserTests
+	{
+		[Test]
+		public void ParseProperties()
+		{
+			NameValueCollection css, html;
+			const string rule = ".selector1, .selector2 { background-color: red; +border: 0; +cellpadding: 0; border: 1px solid black}";
+
+			CssParser.ParseProperties(rule, out css, out html);
+
+			css.ShouldBeEquivalentTo(new NameValueCollection
+				{
+					{"background-color", "red"},
+					{"border", "1px solid black"}
+				});
+
+			html.ShouldBeEquivalentTo(new NameValueCollection
+				{
+					{"border", "0"},
+					{"cellpadding", "0"}
+				});
+		}
+
+		[Test]
+		public void ParseSelectors()
+		{
+			StringCollection selectors = CssParser.ParseSelectors(".selector1, .selector2 { background-color: red; ");
+
+			selectors.Should().BeEquivalentTo(new StringCollection
+				{
+					".selector1",
+					".selector2"
+				});
+		}
+
+		[Test]
+		public void OverlyENcodedValues()
+		{
+			IEnumerable<CssRule> actual = CssParser.SplitIntoIndividualRules(".selector1, .selector2 { background-color: &amp;#39;red&amp;#39;; ");
+
+			var expected = new List<CssRule>()
+			{
+				new CssRule
+				{
+					CssProperties = new NameValueCollection {{"background-color", "'red'"}},
+					Selectors = new StringCollection {".selector1", ".selector2"},
+					HtmlAttributes = new NameValueCollection()
+				}
+			};
+
+			actual.First().ShouldBeEquivalentTo(expected.First());
+		}
+
+
+		[Test]
+		public void ParseSelectorsWithComments()
+		{
+			StringCollection selectors = CssParser.ParseSelectors(".selector1, /* comment */ .selector2 { background-color: red; ");
+
+			selectors.Should().BeEquivalentTo(new StringCollection
+				{
+					".selector1",
+					".selector2"
+				});
+		}
+
+		[Test]
+		public void SplitIntoIndividualRules()
+		{
+			const string css = @".selector1, .selector2 { background-color: red; }
+.selector1, #selector2 { background-color: red; +border: 0; +cellpadding: 0; border: 1px solid black}";
+
+			IEnumerable<CssRule> actual = CssParser.SplitIntoIndividualRules(css);
+			var expected = new List<CssRule>
+				{
+					new CssRule
+						{
+							CssProperties = new NameValueCollection {{"background-color", "red"}},
+							Selectors = new StringCollection {".selector1", ".selector2"},
+							HtmlAttributes = new NameValueCollection()
+						},
+					new CssRule
+						{
+							CssProperties = new NameValueCollection
+								{
+									{"background-color", "red"},
+									{"border", "1px solid black"}
+								},
+							HtmlAttributes = new NameValueCollection
+								{
+									{"border", "0"},
+									{"cellpadding", "0"}
+								},
+							Selectors = new StringCollection {".selector1", "#selector2"}
+						}
+				};
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+	}
+}
---
+++ Khan.Tests/UniteTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Khan.Tests
+{
+	[TestFixture]
+	public class UniteTests
+	{
+		[Test]
+		public void Test()
+		{
+			var unite = new Unite();
+			var source = @"<html><body>
+<style type=""text/css-x"">
+	.red { color: red; }
+	.blue { color: blue; }
+	table.slick { border-collapse: collapse; +border: 0; +cellpadding: 0; +cellspacing: 0; }
+</style>
+<p class=""red"">Should be red</p>
+<p class=""blue"">Should be blue</p>
+<table class=""slick"">
+<tr><td>Lalala</td></tr>
+</table>
+</body></html>";
+
+			var result = unite.Process(source);
+			//result.ShouldBeEquivalentTo("asda");
+		}
+	}
+}
